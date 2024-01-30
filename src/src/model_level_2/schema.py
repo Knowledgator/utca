8b1bc7d7a9f3f11b75c7c6efa90abc9b2e29906d
mvy <mykytayaroshenko@gmail.com>
2024-01-30 13:03:30 +0200

    feat: add template for prompt

---
+++ src/src/model_level_2/schema.py
@@ -0,0 +1,11 @@
+from __future__ import annotations
+# from executable_level_1.schema import InputType
+
+class Prompt:
+    def __init__(self) -> None:
+        pass
+
+
+class PromptTemplate:
+    def from_messages(self) -> PromptTemplate:
+        return self
