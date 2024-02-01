a92386bff524dc0b1838efb8b9819ed1b4516bb7
mvy <mykytayaroshenko@gmail.com>
2024-02-01 22:05:32 +0200

    feat: add token searcher q and a

---
+++ src/implementation/tasks/token_searcher/q_and_a/__test__/test_q_and_a.py
@@ -0,0 +1,17 @@
+from implementation.tasks.token_searcher.q_and_a.task import (
+    TokenSearcherQandATask, TokenSearcherQandAConfig
+)
+
+def test_tokensearcher_general():
+    cfg = TokenSearcherQandAConfig(
+        model_name="knowledgator/UTC-DeBERTa-small",
+        device="cpu"
+    )
+
+    task = TokenSearcherQandATask(cfg)
+    res = task.execute({
+        "question": "Who are the founders of Microsoft?",
+        "text": "Microsoft was founded by Bill Gates and Paul Allen on April 4, 1975 to develop and sell BASIC interpreters for the Altair 8800. During his career at Microsoft, Gates held the positions of chairman, chief executive officer, president and chief software architect, while also being the largest individual shareholder until May 2014."
+    })
+
+    assert "Bill Gates and Paul Allen" == res.output[0].span
