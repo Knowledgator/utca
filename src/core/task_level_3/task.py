ac90ba806ddcff8d8303ca36953c1085ae588eb6
mvy <mykytayaroshenko@gmail.com>
2024-02-02 19:18:30 +0200

    feat: update task definition

--- src/core/task_level_3/task.py
+++ src/core/task_level_3/task.py
@@ -1,39 +1,10 @@
-from abc import ABC, abstractmethod
+from abc import ABC
 
-from src.executable_level_1.executable import Executable
-from src.executable_level_1.schema import InputType, OutputType
-from src.model_level_2.model import Model
+from core.executable_level_1.schema import InputType, OutputType
+from core.model_level_2.model import Model
+from core.model_level_2.schema import ConfigType
 
 # Add abstraction, different tasks per one model
 # Maybe push it inside model
-class Task(Executable[InputType, OutputType], ABC):
-    model: Model[InputType, OutputType]
-    task: str
-
-    def __init__(self, model: Model[InputType, OutputType], taskName: str) -> None:
-        self.taks = taskName
-        self.model = model
-
-
-    @abstractmethod
-    def preprocess(self, input_data: InputType) -> InputType:
-        # add prompt
-        pass
-
-
-    def invoke(self, input_data: InputType) -> OutputType:
-        return self.model.invoke(input_data)
-
-
-    @abstractmethod
-    def postprocess(self, output_data: OutputType) -> OutputType:
-        pass
-
-
-    @abstractmethod
-    def process(self, input_data: InputType):
-        self.postprocess(
-            self.invoke(
-                self.preprocess(input_data)
-            )
-        )
+class Task(Model[ConfigType, InputType, OutputType], ABC):
+    ...
