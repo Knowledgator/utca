8319d7155b0ffc96994ba5b7e2e6d81d4a9bc524
Maksym <maksym@Maksyms-MacBook-Pro.local>
2024-02-06 22:10:01 +0200

    dev: evaluation strategy like interpreter aka engine

--- src/core/executable_level_1/pipeline.py
+++ src/core/executable_level_1/pipeline.py
@@ -1,9 +1,15 @@
-from typing import Any, Union, List
+from typing import  Optional, Union, List
+
 from core.executable_level_1.component import Component
+from core.executable_level_1.custom_exceptions import ExecutionSchemaInvalidFirstComponent
 
 from core.executable_level_1.executable import Executable
 from core.executable_level_1.schema import  Action, AddData, ChangeValue, Config, ConfigType, Input, InputType, MergeData, Output, OutputType, RenameAttribute, RenameAttributeQuery, Transformable
 
+
+STATEMENT = List[Union[Executable[Config, Input, Output], Action]]
+
+PROGRAM = List[STATEMENT]
 
 # execute to execute protocol
 # one to one
@@ -25,19 +31,51 @@ class Protocol():
         return transform
 
 class ExecutionSchema():
-    last_executable: Executable[Config, Input, Output]
-    transformer: Transformable
-    statements: List[Union[List[Executable[Config, Input, Output]], List[Action]]]
+    last_executable: Optional[Executable[Config, Input, Output]] = None
+    actions: List[Action]
+    statements: PROGRAM
+    
     def __init__(self, component: Component) -> None:
         if component is Executable[Config, Input, Output]:
             self.last_executable = component
-
+        else:
+            raise ExecutionSchemaInvalidFirstComponent
     def add(self, comp: Component):
-        pass
+        if comp is Action:
+            self.actions.append(comp)
+        if comp is Executable:
+            if self.last_executable != None:
+                self.create_statement(comp)
+                self.last_executable = None
+            else:
+                self.last_executable = comp
 
+    def create_statement(self, executable: Executable[Config, Input, Output]):
+        new_statement: STATEMENT  = []
+        new_statement.append(self.last_executable) # type: ignore
+        executable_with_actions = new_statement + self.actions
+        executable_with_actions.append(executable)
+    def retieve_program(self):
+        return self.statements
 
+# develop further as game engine ?
 class Evaluator():
-    pass
+    program: PROGRAM
+    state_saver: int 
+    logger: int
+    def __init__(self, schema: ExecutionSchema) -> None:
+        self.schema = schema.retieve_program()
+    def run(self):
+        # execution loop
+        for i, st in enumerate(self.program):
+            self.execute_statement(st)
+            print("Executed step: ", i)
+    def execute_statement(self, statement: STATEMENT):
+        for el in statement:
+            if el is Executable:
+                pass
+            else:
+                pass
 
 
 
@@ -46,23 +84,20 @@ class Evaluator():
 # transformable can NOT be done on static fold
 # how it can dynamic change ?
 
-# Головні питання:
-#  чи наслідується від executable ? + є io + спільний метода і передача
-# 
-# 
-class Pipeline():
-    last_exec: Executable[Config, Input, Output]
-    def __ror__(self, __value: Union[Executable[ConfigType, InputType, OutputType], Transformable])-> int:
-        # add 2 pipelines
-        return 3
-    def __call__(self, *args: Any, **kwds: Any) -> Any:
-        pass
-    def run(self) -> None:
-        pass
-    
-    def run_protocol(self):
 
-        pass
+# class Pipeline():
+#     last_exec: Executable[Config, Input, Output]
+#     def __ror__(self, __value: Union[Executable[ConfigType, InputType, OutputType], Transformable])-> int:
+#         # add 2 pipelines
+#         return 3
+#     def __call__(self, *args: Any, **kwds: Any) -> Any:
+#         pass
+#     def run(self) -> None:
+#         pass
+    
+#     def run_protocol(self):
+
+#         pass
 
 
 
