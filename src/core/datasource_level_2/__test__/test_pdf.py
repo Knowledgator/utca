682c6b2a7e378db8fcbc51a7e1a198341b5262db
mvy <mykytayaroshenko@gmail.com>
2024-02-07 14:11:46 +0200

    feat: add pdf

---
+++ src/core/datasource_level_2/__test__/test_pdf.py
@@ -0,0 +1,25 @@
+import logging
+
+from core.datasource_level_2.pdf import (
+    PDFReadConfig, PDFReadInput,
+    PDFWriteConfig, PDFWriteInput,
+    PDFFile
+)
+
+def test_pdf():
+    file = PDFFile()
+
+    file.write(PDFWriteConfig()).execute(
+        PDFWriteInput(
+            path_to_file='test.pdf',
+            text='In the last few years, the number of programmers concerned about writing structured commit messages have dramatically grown. As exposed by Tim Pope in article readable commit messages are easy to follow when looking through the project history. Moreover the AngularJS contributing guides introduced conventions that can be used by automation tools to automatically generate useful documentation, or by developers during debugging process.\n\nThis document borrows some concepts, conventions and even text mainly from these two sources, extending them in order to provide a sensible guideline for writing commit messages.',
+            y_pending=1
+        )
+    )
+    texts = file.read(PDFReadConfig()).execute(
+        PDFReadInput(
+            path_to_file='test.pdf'
+        )
+    )
+    
+    logging.error(texts)
---
+++ src/core/datasource_level_2/pdf.py
@@ -0,0 +1,114 @@
+from typing import Dict, Any, Type, Optional
+
+import PyPDF2
+from reportlab.pdfgen.canvas import Canvas # type: ignore
+from reportlab.lib.units import cm # type: ignore
+from reportlab.lib.pagesizes import A4 # type: ignore
+
+from core.datasource_level_2.datasource import DatasourceManager, DatasourceAction
+from core.datasource_level_2.schema import (
+    DatasourceConfig,
+    DatasourceInput,
+    DatasourceOutput
+)
+
+class PDFReadConfig(DatasourceConfig):
+    ...
+
+
+class PDFReadInput(DatasourceInput):
+    path_to_file: str
+    pages: Optional[list[int]] = None
+
+
+class PDFReadOutput(DatasourceOutput):
+    texts: Dict[int, str]
+
+
+class PDFRead(DatasourceAction[
+    PDFReadConfig,
+    PDFReadInput,
+    PDFReadOutput
+]):
+    input_class: Type[PDFReadInput] = PDFReadInput 
+    output_class: Type[PDFReadOutput] = PDFReadOutput
+
+    def invoke(self, input_data: PDFReadInput) -> Dict[str, Any]:
+        with open(input_data.path_to_file, 'rb') as f:
+            pdfReader = PyPDF2.PdfReader(f)
+            pages = pdfReader.pages
+            texts = {
+                idx: pages[idx].extract_text()
+                for idx in (input_data.pages or range(len(pages)))
+            }
+        return {'texts': texts}
+
+
+    def invoke_batch(self, input_data: list[PDFReadInput]) -> list[Dict[str, Any]]:
+        return [self.invoke(i) for i in input_data]
+
+
+class PDFWriteConfig(DatasourceConfig):
+    ...
+
+
+class PDFWriteInput(DatasourceInput):
+    path_to_file: str
+    text: str
+    x_pending: float=1
+    y_pending: float=1
+    page_width: float = A4[0]
+    page_height: float = A4[1]
+
+
+class PDFWriteOutput(DatasourceOutput):
+    ...
+
+
+class PDFWrite(DatasourceAction[
+    PDFWriteConfig,
+    PDFWriteInput,
+    PDFWriteOutput
+]):
+    input_class: Type[PDFWriteInput] = PDFWriteInput 
+    output_class: Type[PDFWriteOutput] = PDFWriteOutput
+    
+    def invoke(self, input_data: PDFWriteInput) -> Dict[str, Any]:
+        canvas = Canvas(
+            input_data.path_to_file, 
+            pagesize=(input_data.page_width, input_data.page_height)
+        )
+        canvas.drawString( # type: ignore
+            input_data.x_pending*cm, 
+            input_data.page_height - input_data.y_pending*cm, 
+            input_data.text
+        ) 
+        canvas.save()
+        return {}
+
+
+    def invoke_batch(self, input_data: list[PDFWriteInput]) -> list[Dict[str, Any]]:
+        for i in input_data:
+            self.invoke(i)
+        return []
+
+
+class PDFFile(DatasourceManager[
+    PDFReadConfig,
+    PDFReadInput,
+    PDFReadOutput,
+
+    PDFWriteConfig,
+    PDFWriteInput,
+    PDFWriteOutput,
+]):
+    def read(
+        self, cfg: PDFReadConfig,
+    ) -> PDFRead:
+        return PDFRead(cfg)
+
+    
+    def write(
+        self, cfg: PDFWriteConfig,
+    ) -> PDFWrite:
+        return PDFWrite(cfg)
