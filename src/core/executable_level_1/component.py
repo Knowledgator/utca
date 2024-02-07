ff3f33dafc1f661cdac346846ef58d527569cf0b
mvy <mykytayaroshenko@gmail.com>
2024-02-07 17:38:25 +0200

    feat: add or

--- src/core/executable_level_1/component.py
+++ src/core/executable_level_1/component.py
@@ -1,10 +1,22 @@
+from __future__ import annotations
 from typing import TYPE_CHECKING, Union
 
 if TYPE_CHECKING:
     from core.executable_level_1.eval import ExecutionSchema  # Forward declaration for type checking
 
 class Component:
-    def __ror__(self, comp: Union['ExecutionSchema', 'Component']) -> 'ExecutionSchema':
+    def __or__(self, comp: Component) -> ExecutionSchema:
+        from core.executable_level_1.eval import ExecutionSchema
+        if isinstance(self, ExecutionSchema):
+            self.add(comp)
+            return self
+        else:
+            e = ExecutionSchema(self)
+            e.add(comp)
+            return e
+    
+    
+    def __ror__(self, comp: Union[ExecutionSchema, Component]) -> ExecutionSchema:
         from core.executable_level_1.eval import ExecutionSchema
         if isinstance(comp, ExecutionSchema):
             comp.add(self)
