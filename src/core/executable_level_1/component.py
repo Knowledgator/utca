1ef34e15a27634a478be56598d6e9053cdcd115f
Maksym <maksym@Maksyms-MacBook-Pro.local>
2024-02-07 17:20:59 +0200

    fix: fix component __ror__

--- src/core/executable_level_1/component.py
+++ src/core/executable_level_1/component.py
@@ -1,15 +1,17 @@
 from typing import TYPE_CHECKING, Union
 
 if TYPE_CHECKING:
-    from core.executable_level_1.pipeline import ExecutionSchema  # Forward declaration for type checking
+    from core.executable_level_1.eval import ExecutionSchema  # Forward declaration for type checking
 
 class Component:
     def __ror__(self, comp: Union['ExecutionSchema', 'Component']) -> 'ExecutionSchema':
-        from core.executable_level_1.pipeline import ExecutionSchema
+        from core.executable_level_1.eval import ExecutionSchema
         if isinstance(comp, ExecutionSchema):
             comp.add(self)
             return comp
         else:
-            return ExecutionSchema(self)
+            e = ExecutionSchema(comp)
+            e.add(self)
+            return e
 
 
