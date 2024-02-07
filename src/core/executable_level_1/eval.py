68bf1d466bbfaa3b128b7160187198467e81a491
mvy <mykytayaroshenko@gmail.com>
2024-02-07 20:41:24 +0200

    fix: some issues

--- src/core/executable_level_1/eval.py
+++ src/core/executable_level_1/eval.py
@@ -4,8 +4,19 @@ from core.executable_level_1.component import Component
 from core.executable_level_1.custom_exceptions import ExecutionSchemaInvalidFirstComponent, ExecutionSchemaInvalidFlow
 
 from core.executable_level_1.executable import Executable
-from core.executable_level_1.schema import  Action, AddData, ChangeValue, Config, Input,  MergeData, Output,RenameAttribute, RenameAttributeQuery, Transformable
-
+from core.executable_level_1.schema import (
+    Action, 
+    AddData, 
+    ChangeValue, 
+    Config, 
+    Input,
+    MergeData,
+    Output,
+    RenameAttribute,
+    RenameAttributeQuery, 
+    Transformable,
+    ExecuteFunction
+)
 
 TRANSFORM_STATEMENT = List[Union[Executable[Config, Input, Output], Action]]
 INPUT_STATEMENT = List[Executable[Config, Input, Output]] # Executable Executable Executable
@@ -34,37 +45,58 @@ class Protocol():
             transform.merge_state(action.get_data(), action.is_new_priority())
         elif isinstance(action, ChangeValue):
             transform.change_value(action.get_key(), action.get_value())
+        elif isinstance(action, ExecuteFunction):
+            transform.custom_tranform(action.func)
         return transform
+
 
 class ExecutionSchema():
     last_executable: Optional[Executable[Config, Input, Output]] = None
     actions: List[Action]
     statements: PROGRAM
+
     def __init__(self, comp: Component) -> None:
-        if comp is Executable[Config, Input, Output]:
+        self.statements = []
+        self.actions = []
+        if isinstance(comp, Executable):
             self.last_executable = comp
             self.create_input_statement(comp)
         else:
             raise ExecutionSchemaInvalidFirstComponent
+        
+
     def add(self, comp: Component):
-        if comp is Action:
+        if isinstance(comp, Action):
             self.actions.append(comp)
-        if comp is Executable:
+        elif isinstance(comp, Executable):
             if self.last_executable != None:
                 self.create_statement(comp)
                 self.last_executable = comp
+                self.actions = []
             else:
-                raise ExecutionSchemaInvalidFlow
-    def create_input_statement(self, comp: Executable[Config, Input, Output]):
-         new_statement: STATEMENT  = []
-         new_statement.append(comp)
-         self.statements.append(new_statement)
-    def create_statement(self, executable: Executable[Config, Input, Output]):
+                raise ExecutionSchemaInvalidFlow()
+        else:
+            raise ValueError(comp)
+    
+    
+    def create_input_statement(
+        self, comp: Executable[Config, Input, Output]
+    ):
+        new_statement: STATEMENT  = []
+        new_statement.append(comp)
+        self.statements.append(new_statement)
+
+
+    def create_statement(
+        self, executable: Executable[Config, Input, Output]
+    ):
         # EXECUTABLE + ACTION
         new_statement: STATEMENT  = []
         new_statement = new_statement + self.actions
         new_statement.append(executable)
         self.statements.append(new_statement)
+    
+    
     def retieve_program(self):
         return self.statements
 
@@ -77,24 +109,29 @@ class EvaluatorConfigs():
 class Evaluator():
     program: PROGRAM
     transferable_checkpoint: Transformable
+    
     def __init__(self, schema: ExecutionSchema, cfg: EvaluatorConfigs = EvaluatorConfigs()) -> None:
-        self.schema: PROGRAM = schema.retieve_program()
+        self.program = schema.retieve_program()
+    
+    
     def run(self, program_input: Input):
         # execution loop
-
         for i, st in enumerate(self.program):
-            if st is INPUT_STATEMENT:
+            if i == 0:
                 self.execute_input_statement(st, program_input)
-            if st is TRANSFORM_STATEMENT:
+            else:
                 self.execute_ordinary_statement(st)
             print("Executed step: ", i)
         return self.transferable_checkpoint.extract()
+    
+
     def execute_ordinary_statement(self, statement: TRANSFORM_STATEMENT):
         for el in statement:
             if  isinstance(el, Executable):
                 self.transferable_checkpoint = el.execute(self.transferable_checkpoint, Transformable)
             else:
                 Protocol.handle_transform(self.transferable_checkpoint, el)
+
 
     def execute_input_statement(self, statement: INPUT_STATEMENT, input: Input):
         executable = statement[0]
