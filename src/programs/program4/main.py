33c6e20d12e154a4ed0f5531fdcfd0c14776a993
mvy <mykytayaroshenko@gmail.com>
2024-02-20 16:13:18 +0200

    refactor: change preloaded labels to labels from model

--- src/programs/program4/main.py
+++ src/programs/program4/main.py
@@ -1,9 +1,9 @@
 from typing import Dict, Any, cast
-import json
 
 from transformers import ( # type: ignore
     AutoFeatureExtractor, 
-    AutoModelForImageClassification
+    AutoModelForImageClassification,
+    AutoConfig
 )
 
 from core.model_level_2.transformers_image_classification import (
@@ -19,6 +19,8 @@ from core.executable_level_1.interpreter import Evaluator
 
 model_name = "facebook/deit-base-distilled-patch16-384"
 
+labels = AutoConfig.from_pretrained(model_name).id2label # type: ignore
+
 # Define model stage
 model_stage = TransformersImageClassification( # type: ignore
     TransformersImageClassificationConfig(
@@ -28,8 +30,6 @@ model_stage = TransformersImageClassification( # type: ignore
 )
 
 def interpret_results(model_ouput: Dict[str, Any]) -> Dict[str, Any]:
-    with open("programs/program4/test_labels.json", "r") as f:
-        labels = json.load(f)
     predicted_class_idx = model_ouput["outputs"]["logits"].argmax().item()
     return {"label": labels[predicted_class_idx]}
 
