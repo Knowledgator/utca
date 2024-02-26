0a02baf350dbd353055eee927a6db483ddfcdd43
mvy <mykytayaroshenko@gmail.com>
2024-02-26 17:27:33 +0200

    feat: add json actions

---
+++ src/implementation/datasources/json/actions.py
@@ -0,0 +1,18 @@
+from typing import Dict, Any
+import json
+
+from core.executable_level_1.actions import Action
+
+class JSONRead(Action):
+    def execute(self, input_data: Dict[str, Any]) -> Dict[str, Any]:
+        with open(input_data["path_to_file"], "r") as f:
+            return {
+                "json": json.load(f)
+            }
+
+
+class JSONWrite(Action):
+    def execute(self, input_data: Dict[str, Any]) -> Dict[str, Any]:
+        with open(input_data["path_to_file"], "w") as f:
+            json.dump(input_data["json"], f)
+        return input_data
