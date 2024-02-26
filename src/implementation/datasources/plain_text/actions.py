7728aaf19adbdc65f61dd5a5582b1f275878d556
mvy <mykytayaroshenko@gmail.com>
2024-02-26 17:24:48 +0200

    feat: add plain text actions

---
+++ src/implementation/datasources/plain_text/actions.py
@@ -0,0 +1,24 @@
+from typing import Dict, Any
+
+from core.executable_level_1.actions import Action
+
+class PlainTextRead(Action):
+    def execute(self, input_data: Dict[str, Any]) -> Dict[str, Any]:
+        with open(input_data["path_to_file"], "r") as f:
+            return {
+                "text": f.read()
+            }
+
+
+class PlainTextWrite(Action):
+    def execute(self, input_data: Dict[str, Any]) -> Dict[str, Any]:
+        with open(input_data["path_to_file"], "w") as f:
+            f.write(input_data["text"])
+        return input_data
+    
+
+class PlainTextAppend(Action):
+    def execute(self, input_data: Dict[str, Any]) -> Dict[str, Any]:
+        with open(input_data["path_to_file"], "a") as f:
+            f.write(input_data["text"])
+        return input_data
