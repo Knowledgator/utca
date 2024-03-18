f2a07e819d4d973c70011da04edab35f6b9eed4f
mvy <mykytayaroshenko@gmail.com>
2024-03-18 14:05:17 +0200

    refactor: pdf actions

--- src/implementation/datasources/pdf/actions.py
+++ src/implementation/datasources/pdf/actions.py
@@ -1,4 +1,4 @@
-from typing import Dict, Any, Iterator, cast
+from typing import Dict, Any, List, Iterator
 import io
 
 from PIL import Image
@@ -7,72 +7,75 @@ from reportlab.lib.units import cm # type: ignore
 from reportlab.lib.pagesizes import A4 # type: ignore
 
 from core.executable_level_1.actions import Action
-from implementation.datasources.pdf.custom_pdf import CustomPDF, Page
+from implementation.datasources.pdf.custom_pdf import CustomPDF, Page, Table
 
-class PDFRead(Action[Dict[str, Any], Dict[str, Any]]):
-    def execute(self, input_data: Dict[str, Any]) -> Dict[str, Any]:
+class PDFRead(Action[Dict[str, Any], Dict[int, Page]]):
+    def execute(self, input_data: Dict[str, Any]) -> Dict[int, Page]:
         pdfReader = CustomPDF.open(
             input_data['path_to_file'], 
             pages=input_data.get('pages')
         )
-        input_data["pdf"] = {
-            "pages": {
-                page.page_number: page
-                for page in pdfReader.pages
-            }
+        return {
+            page.page_number: page
+            for page in pdfReader.pages
         }
-        return input_data
 
 
-class PDFExtractTexts(Action[Dict[str, Any], Dict[str, Any]]):
-    def execute(self, input_data: Dict[str, Any]) -> Dict[str, Any]:
-        input_data["pdf"]["texts"] = {
-            page_id: cast(Page, page).extract_text(tables=input_data.get("tables"))
-            for page_id, page in input_data["pdf"]["pages"].items()
+class PDFExtractTexts(Action[Dict[int, Page], Dict[int, str]]):
+    def __init__(self, tables: bool=True) -> None:
+        self.tables = tables
+    
+
+    def execute(self, input_data: Dict[int, Page]) -> Dict[int, str]:
+        return {
+            page_id: page.extract_text(tables=self.tables)
+            for page_id, page in input_data.items()
         }
-        return input_data
 
 
-class PDFFindTables(Action[Dict[str, Any], Dict[str, Any]]):
-    def execute(self, input_data: Dict[str, Any]) -> Dict[str, Any]:
-        input_data["pdf"]["tables"] = {
-            page_id: cast(Page, page).find_tables()
-            for page_id, page in input_data["pdf"]["pages"].items()
+class PDFFindTables(Action[Dict[int, Page], Dict[int, List[Table]]]):
+    def execute(self, input_data: Dict[int, Page]) -> Dict[int, List[Table]]:
+        return {
+            page_id: page.find_tables()
+            for page_id, page in input_data.items()
         }
-        return input_data
 
 
-class PDFExtractTables(Action[Dict[str, Any], Dict[str, Any]]):
-    def execute(self, input_data: Dict[str, Any]) -> Dict[str, Any]:
-        input_data["pdf"]["extracted_tables"] = {
-            page_id: cast(Page, page).extract_tables()
-            for page_id, page in input_data["pdf"]["pages"].items()
+class PDFExtractTables(Action[Dict[int, Page], Dict[int, Any]]):
+    def execute(self, input_data: Dict[int, Page]) -> Dict[int, Any]:
+        return {
+            page_id: page.extract_tables()
+            for page_id, page in input_data.items()
         }
-        return input_data
             
 
-class PDFExtractImages(Action[Dict[str, Any], Dict[str, Any]]):
-    def extract_images_from_page(self, page: Page) -> Iterator[Image.Image]:
+class PDFExtractImages(Action[Dict[int, Page], Dict[int, Any]]):
+    resolution: int 
+    def __init__(self, resolution: int=72) -> None:
+        self.resolution = resolution
+    
+    def extract_images_from_page(self, page: Page, resolution: int) -> Iterator[Image.Image]:
         page_height = page.height
 
         for image in page.images:
             image_bbox = (image['x0'], page_height - image['y1'], image['x1'], page_height - image['y0'])
             cropped_page = page.crop(image_bbox)
             tmp = io.BytesIO()
-            cropped_page.to_image(resolution=1024).save(tmp)
+            cropped_page.to_image(resolution=resolution).save(tmp)
             yield Image.open(tmp)
 
 
-    def execute(self, input_data: Dict[str, Any]) -> Dict[str, Any]:
-        images: Dict[str, Any] = {}
-        for page_id, page in input_data["pdf"]["pages"].items():
-            images[page_id] = list(self.extract_images_from_page(cast(Page, page)))
-        input_data["pdf"]["images"] = images
-        return input_data
+    def execute(self, input_data: Dict[int, Page]) -> Dict[int, Any]:
+        return {
+            page_id: list(self.extract_images_from_page(
+                page, 
+                resolution=self.resolution
+            )) for page_id, page in input_data.items()
+        }
 
 
-class PDFWrite(Action[Dict[str, Any], Dict[str, Any]]):
-    def execute(self, input_data: Dict[str, Any]) -> Dict[str, Any]:
+class PDFWrite(Action[Dict[str, Any], None]):
+    def execute(self, input_data: Dict[str, Any]) -> None:
         canvas = Canvas(
             input_data['path_to_file'], 
             pagesize=(
@@ -86,4 +89,3 @@ class PDFWrite(Action[Dict[str, Any], Dict[str, Any]]):
             input_data['text']
         ) 
         canvas.save()
-        return input_data
