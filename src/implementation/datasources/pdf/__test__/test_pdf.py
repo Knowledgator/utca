4c9fe5a34555b7a829cf1bf48a76a85a978fb942
mvy <mykytayaroshenko@gmail.com>
2024-03-19 20:10:44 +0200

    refactor: pdf test

--- src/implementation/datasources/pdf/__test__/test_pdf.py
+++ src/implementation/datasources/pdf/__test__/test_pdf.py
@@ -1,4 +1,6 @@
 import logging
+import pathlib
+PATH = pathlib.Path(__file__).parent.resolve()
 
 from implementation.datasources.pdf.actions import (
     PDFRead,
@@ -7,13 +9,13 @@ from implementation.datasources.pdf.actions import (
 
 def test_pdf():
     PDFWrite().execute({
-        "path_to_file": "test.pdf",
+        "path_to_file": f"{PATH}/test.pdf",
         "text": "In the last few years, the number of programmers concerned about writing structured commit messages have dramatically grown. As exposed by Tim Pope in article readable commit messages are easy to follow when looking through the project history. Moreover the AngularJS contributing guides introduced conventions that can be used by automation tools to automatically generate useful documentation, or by developers during debugging process.\n\nThis document borrows some concepts, conventions and even text mainly from these two sources, extending them in order to provide a sensible guideline for writing commit messages.",
         "y_pending": 1
     })
 
     texts = PDFRead().execute({
-        "path_to_file": "test.pdf"
+        "path_to_file": f"{PATH}/test.pdf"
     })
     
     logging.error(texts)
