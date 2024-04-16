cb9a1421bb4647f122cb8008f9f483c3cb989ae2
mvy <mykytayaroshenko@gmail.com>
2024-04-16 20:11:34 +0300

    fix: extend imports

--- src/implementation/tasks/__init__.py
+++ src/implementation/tasks/__init__.py
@@ -1,81 +1,201 @@
+# Audio processing
 from implementation.tasks.audio_processing.text_to_speech.transformers.transformers_text_to_speech import (
-    TransformersTextToSpeech
+    TransformersTextToSpeech,
 )
-from implementation.tasks.text_processing.summarization.transformers.transformers_summarization import (
-    TransformersTextSummarization
+
+# Image processing
+from implementation.tasks.image_processing.charts_and_plots_analysis.transformers.transformers_charts_and_plots_analysis import (
+    TransformersChartsAndPlotsAnalysis,
+    ChartsAndPlotsAnalysisInput,
 )
+from implementation.tasks.image_processing.charts_and_plots_analysis.transformers.actions import (
+    ChartsAndPlotsAnalysisPreprocessor,
+    ChartsAndPlotsAnalysisPostprocessor,
+)
+
 from implementation.tasks.image_processing.documents_q_and_a.transformers.transformers_layout_lm import (
-    TransformersDocumentQandA
+    TransformersDocumentQandA,
+    DocumentQandAOutput,
 )
+from implementation.tasks.image_processing.documents_q_and_a.transformers.actions import (
+    DocumentQandAPostprocess,
+)
+
 from implementation.tasks.image_processing.image_classification.transformers.transformers_image_classification import (
     TransformersImageClassification,
+    TransformersImageClassificationInput,
     TransformersImageClassificationOutput,
-    TransformersImageClassificationMultilabelOutput
+    TransformersImageClassificationMultilabelOutput,
 )
 from implementation.tasks.image_processing.image_classification.transformers.actions import (
     ImageClassificationPreprocessor,
     ImageClassificationSingleLabelPostprocessor,
-    ImageClassificationMultilabelPostprocessor
+    ImageClassificationMultilabelPostprocessor,
 )
-from implementation.tasks.image_processing.charts_and_plots_analysis.transformers.transformers_charts_and_plots_analysis import (
-    TransformersChartsAndPlotsAnalysis
-)
+
 from implementation.tasks.image_processing.visual_q_and_a.transformers.transformers_visual_q_and_a import (
-    TransformersVisualQandA
+    TransformersVisualQandA,
+    TransformersVisualQandAInput,
+    TransformersVisualQandAOutput,
+    TransformersVisualQandAMultianswerOutput,
 )
-from implementation.tasks.text_processing.textual_q_and_a.transformers.transformers_q_and_a import (
-    TransformersTextualQandA
+from implementation.tasks.image_processing.visual_q_and_a.transformers.actions import (
+    VisualQandAPreprocessor,
+    VisualQandAMultianswerPostprocessor,
+    VisualQandASingleAnswerPostprocessor,
 )
-from implementation.tasks.text_processing.entity_linking.transformers.transformers_entity_linking import (
-    TransformersEntityLinking
-)
-from implementation.tasks.text_processing.text_classification.comprehend_it.comprehend_it import (
-    ComprehendIt
-)
-from implementation.tasks.text_processing.ner.transformers.transformers_token_classification import (
-    TransformersTokenClassifier
-)
+
+# Text processing
 from implementation.tasks.text_processing.clean_text.token_searcher.token_searcher import (
-    TokenSearcherTextCleaner
+    TokenSearcherTextCleaner,
+    TokenSearcherTextCleanerInput,
+    TokenSearcherTextCleanerOutput,
 )
 from implementation.tasks.text_processing.clean_text.token_searcher.actions import (
+    TokenSeatcherTextCleanerPreprocessor,
     TokenSearcherTextCleanerPostprocessor,
 )
+
+from implementation.tasks.text_processing.embedding.transformers.transformers_embedding import (
+    TextEmbedding,
+    TextEmbeddingInput, 
+    TextEmbeddingOutput,
+)
+from implementation.tasks.text_processing.embedding.transformers.actions import (
+    EmbeddingPreprocessor,
+    EmbeddingPostprocessor,
+    ConvertEmbeddingsToNumpyArrays,
+)
+
+from implementation.tasks.text_processing.entity_linking.transformers.transformers_entity_linking import (
+    TransformersEntityLinking,
+    EntityLinkingInput, 
+    EntityLinkingOutput,
+)
+from implementation.tasks.text_processing.entity_linking.transformers.actions import (
+    EntityLinkingPreprocessing,
+    EntityLinkingPostprocess,
+)
+
 from implementation.tasks.text_processing.ner.token_searcher.token_searcher import (
-    TokenSearcherNER
+    TokenSearcherNER,
+    TokenSearcherNERInput,
+    TokenSearcherNEROutput,
 )
 from implementation.tasks.text_processing.ner.token_searcher.actions import (
+    TokenSearcherNERPreprocessor,
     TokenSearcherNERPostprocessor,
 )
+
+from implementation.tasks.text_processing.ner.transformers.transformers_token_classification import (
+    TransformersTokenClassifier,
+    TransformersTokenClassifierOutput,
+)
+from implementation.tasks.text_processing.ner.transformers.actions import (
+    TokenClassifierPostprocessor,
+)
+
+from implementation.tasks.text_processing.summarization.transformers.transformers_summarization import (
+    TransformersTextSummarization,
+    SummarizationOutput,
+)
+from implementation.tasks.text_processing.summarization.transformers.actions import (
+    SummarizationPostprocess,
+)
+
+from implementation.tasks.text_processing.text_classification.comprehend_it.comprehend_it import (
+    ComprehendIt,
+)
+
 from implementation.tasks.text_processing.textual_q_and_a.token_searcher.token_searcher import (
-    TokenSearcherQandATask
+    TokenSearcherQandATask,
+    TokenSearcherQandAInput,
+    TokenSearcherQandAOutput,
 )
 from implementation.tasks.text_processing.textual_q_and_a.token_searcher.actions import (
+    TokenSearcherQandAPreprocessor,
     TokenSearcherQandAPostprocessor,
 )
 
+from implementation.tasks.text_processing.textual_q_and_a.transformers.transformers_q_and_a import (
+    TransformersTextualQandA
+)
+from implementation.tasks.text_processing.textual_q_and_a.transformers.actions import (
+    QandAPostprocess
+)
+
 __all__ = [
+    # Audio processing
     "TransformersTextToSpeech",
-    "TransformersTextSummarization",
+
+    # Image processing
+    "TransformersChartsAndPlotsAnalysis",
+    "ChartsAndPlotsAnalysisInput",
+    "ChartsAndPlotsAnalysisPreprocessor",
+    "ChartsAndPlotsAnalysisPostprocessor",
+
     "TransformersDocumentQandA",
-    
+    "DocumentQandAOutput",
+    "DocumentQandAPostprocess",
+
     "TransformersImageClassification",
+    "TransformersImageClassificationInput",
     "TransformersImageClassificationOutput",
     "TransformersImageClassificationMultilabelOutput",
     "ImageClassificationPreprocessor",
     "ImageClassificationSingleLabelPostprocessor",
     "ImageClassificationMultilabelPostprocessor",
 
-    "TransformersChartsAndPlotsAnalysis",
     "TransformersVisualQandA",
-    "TransformersTextualQandA",
-    "TransformersEntityLinking",
-    "ComprehendIt",
-    "TransformersTokenClassifier",
+    "TransformersVisualQandAInput",
+    "TransformersVisualQandAOutput",
+    "TransformersVisualQandAMultianswerOutput",
+    "VisualQandAPreprocessor",
+    "VisualQandAMultianswerPostprocessor",
+    "VisualQandASingleAnswerPostprocessor",
+
+    # Text processing
     "TokenSearcherTextCleaner",
+    "TokenSearcherTextCleanerInput",
+    "TokenSearcherTextCleanerOutput",
+    "TokenSeatcherTextCleanerPreprocessor",
     "TokenSearcherTextCleanerPostprocessor",
+    
+    "TextEmbedding",
+    "TextEmbeddingInput", 
+    "TextEmbeddingOutput",
+    "EmbeddingPreprocessor",
+    "EmbeddingPostprocessor",
+    "ConvertEmbeddingsToNumpyArrays",
+
+    "TransformersEntityLinking",
+    "EntityLinkingInput", 
+    "EntityLinkingOutput",
+    "EntityLinkingPreprocessing",
+    "EntityLinkingPostprocess",
+
     "TokenSearcherNER",
     "TokenSearcherNERPostprocessor",
+    "TokenSearcherNERInput",
+    "TokenSearcherNEROutput",
+    "TokenSearcherNERPreprocessor",
+
+    "TransformersTokenClassifier",
+    "TransformersTokenClassifierOutput",
+    "TokenClassifierPostprocessor",
+
+    "TransformersTextSummarization",
+    "SummarizationOutput",
+    "SummarizationPostprocess",
+
+    "ComprehendIt",
+
     "TokenSearcherQandATask",
+    "TokenSearcherQandAInput",
+    "TokenSearcherQandAOutput",
+    "TokenSearcherQandAPreprocessor",
     "TokenSearcherQandAPostprocessor",
+
+    "TransformersTextualQandA",
+    "QandAPostprocess",
 ]
