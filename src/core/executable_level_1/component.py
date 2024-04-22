0eb320b5659c5fd5ba1c28454dbc96423625b41e
mvy <mykytayaroshenko@gmail.com>
2024-04-22 23:46:06 +0300

    refactor: add doc strings

--- src/core/executable_level_1/component.py
+++ src/core/executable_level_1/component.py
@@ -8,16 +8,42 @@ if TYPE_CHECKING:
     from core.executable_level_1.interpreter import Evaluator
 
 class Component(ABC):
+    """
+    Base class for data processing
+    """
     def __init__(self, name: Optional[str]=None):
+        """
+        Args:
+            name (Optional[str], optional): Name for identification.
+                If equals to None, class name will be used. Defaults to None.
+        """
         self._name = name or self.__class__.__name__
 
 
     def __or__(self, comp: Component) -> ExecutionSchema:
+        """
+        Creates ExecutionSchema from two components
+
+        Args:
+            comp (Component): Component that will be added 
+                with this component to ExecutionSchema.
+
+        Returns:
+            ExecutionSchema: Result of combination of two components.
+                When schema will be called, this component will be executed first.
+        """
         from core.executable_level_1.eval import ExecutionSchema
         return ExecutionSchema(self).add(comp)
 
 
     def set_up_default_evaluator(self) -> Evaluator:
+        """
+        Creates default Evaluator.
+
+        Returns:
+            Evaluator: Evaluator that will be used as a context 
+                of execution of this component.
+        """
         from core.executable_level_1.interpreter import Evaluator
         return Evaluator(self)
 
@@ -26,13 +52,34 @@ class Component(ABC):
     def __call__(
         self, input_data: Transformable, evaluator: Optional[Evaluator]=None
     ) -> Transformable:
+        """
+        Component Call
+
+        Args:
+            input_data (Transformable): Data for processing
+
+            evaluator (Optional[Evaluator], optional): Evaluator in context of wich component executed.
+                If equals to None, default evaluator will be created. Defaults to None.
+
+        Returns:
+            Transformable: Result of executed component.
+        """
         ...
 
 
     def prepare_input(
         self, input_data: Optional[Dict[str, Any]]=None
     ) -> Transformable:
-        """Sets the initial input for the program."""
+        """
+        Sets the initial input for the program.
+
+        Args:
+            input_data (Optional[Dict[str, Any]], optional): Data for processing. 
+                If equals to None, empty dict will be used for input_data. Defaults to None.
+
+        Returns:
+            Transformable: Initial input.
+        """
         if input_data is not None:
             return Transformable(input_data)
         return Transformable({})
@@ -40,16 +87,36 @@ class Component(ABC):
 
     def run(
         self, input_data: Optional[Dict[str, Any]]=None
-    ) -> Dict[str, Any]:    
+    ) -> Dict[str, Any]:
+        """
+        Run Component
+
+        Args:
+            input_data (Optional[Dict[str, Any]], optional): Data for processing. 
+                If equals to None, empty dict will be used for input_data. Defaults to None.
+
+        Returns:
+            Dict[str, Any]: Result of execution.
+        """
         return self(self.prepare_input(input_data)).extract()
 
     
     @property
     def name(self) -> str:
+        """
+        Name for identification
+        """
         return self._name
     
 
     def set_name(self, name: str) -> Component:
+        """
+        Args:
+            name (str): New name.
+
+        Returns:
+            Component: self
+        """
         self._name = name
         return self
     
