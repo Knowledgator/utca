3f8d64374e265972cdc3ee24010a4c96862b93cf
mvy <mykytayaroshenko@gmail.com>
2024-04-23 15:25:33 +0300

    refactor: add docstrings

--- src/core/predictor_level_2/predictor.py
+++ src/core/predictor_level_2/predictor.py
@@ -5,7 +5,13 @@ from core.executable_level_1.executable import Executable
 from core.executable_level_1.schema import Input, Output
 
 class Predictor(Executable[Input, Output]):
+    """
+    Base predictor class
+    """
     @property
     @abstractmethod
     def config(self) -> Any:
+        """
+        Predictor configuration
+        """
         ...
