9820adcb545dcd5d9002e2de8b7dc5417a7ca5c7
mvy <mykytayaroshenko@gmail.com>
2024-04-23 17:55:44 +0300

    refactor: add docstrings

--- src/implementation/conditions/semantic/embedding.py
+++ src/implementation/conditions/semantic/embedding.py
@@ -12,6 +12,9 @@ from implementation.schemas.semantic_search_schema import (
 )
 
 class SemanticCondition(Condition):
+    """
+    Semantic condition
+    """
     def __init__(
         self,
         *,
@@ -21,6 +24,22 @@ class SemanticCondition(Condition):
         subject_key: str,
         name: Optional[str]=None
     ) -> None:
+        """
+        Args:
+            distance (float): Distance thresholde.
+            
+            targets (List[str]): Comparison targets.
+            
+            subject_key (str): Key associated with source of subjects to match.
+                Source should be a list of strings.
+            
+            semantic_schema (Optional[SemanticSearchSchema], optional): Schema that will be used
+                for distance evaluation. If equals to None, default SemanticSearchSchema
+                will be used. Defaults to None.
+            
+            name (Optional[str], optional): Name for identification.
+                If equals to None, class name will be used. Defaults to None.
+        """
         if semantic_schema is None:
             semantic_schema = SemanticSearchSchema()
         self.semantic_schema = semantic_schema
@@ -38,6 +57,21 @@ class SemanticCondition(Condition):
         targets: List[str],
         semantic_schema: SemanticSearchSchema
     ) -> ExecutionSchema:
+        """
+        Builds evaluation schema
+
+        Args:
+            subject_key (str): Key associated with source of subjects to match.
+                Source should be a list of strings.
+
+            targets (List[str]): Comparison targets.
+            
+            semantic_schema (SemanticSearchSchema): Schema that will be used
+                for distance evaluation.
+
+        Returns:
+            ExecutionSchema: Schema for distance evaluation.
+        """
         def build_index(input_data: Dict[str, Any]) -> Dict[str, Any]:
             semantic_schema.add([input_data[subject_key]])
             return input_data
@@ -54,7 +88,24 @@ class SemanticCondition(Condition):
     
     @classmethod
     def validator_wrapper(cls, distance: float):
+        """
+        Create validator for Condition
+
+        Args:
+            distance (float): Distance thresholde.
+        """
         def validator(input_data: Transformable, evaluator: Evaluator) -> bool:
+            """
+            Validator for condition
+
+            Args:
+                input_data (Transformable): Data to process.
+
+                evaluator (Evaluator): Evaluator in context of wich executed.
+
+            Returns:
+                bool: Result of evaluation. Define that condition is fulfilled or not.
+            """
             return any(
                 res_distance[0] <= distance
                 for res_distance in
@@ -66,5 +117,14 @@ class SemanticCondition(Condition):
     def __call__(
         self, input_data: Transformable, evaluator: Evaluator
     ) -> bool:
+        """
+        Args:
+            input_data (Transformable): Data for processing
+            
+            evaluator (Evaluator): Evaluator in context of wich Condition executed.
+
+        Returns:
+            bool: Result of evaluation. Define that condition is fulfilled or not.
+        """
         self.semantic_schema.drop_index()
         return super()(input_data, evaluator)
