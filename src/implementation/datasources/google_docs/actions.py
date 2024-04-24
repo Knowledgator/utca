bdd2a898c7d412e7f5d8f78388eb7a1e5ac77ee7
mvy <mykytayaroshenko@gmail.com>
2024-04-24 15:20:36 +0300

    refactor: add docstrings and normalize keys names

--- src/implementation/datasources/google_docs/actions.py
+++ src/implementation/datasources/google_docs/actions.py
@@ -28,17 +28,42 @@ from implementation.apis.google_cloud.client import (
 # ]
 
 class GoogleDocsAction(Action[ActionInput, ActionOutput]):
+    """
+    Base Google Documents action
+    """
     def __init__(
         self, 
         client: GoogleCloudClient,
         name: Optional[str]=None,
     ) -> None:
+        """
+        Args:
+            client (GoogleCloudClient): Google client that will be used for acess.
+
+            name (Optional[str], optional): name (Optional[str], optional): Name for identification.
+                If equals to None, class name will be used. Defaults to None.
+        """
         super().__init__(name)
         self.docs_service = client.service.documents()
 
 
 class GoogleDocsCreate(GoogleDocsAction[Dict[str, Any], Dict[str, Any]]):
+    """
+    Create document
+    """
     def execute(self, input_data: Dict[str, Any]) -> Dict[str, Any]:
+        """
+        Args:
+            input_data (Dict[str, Any]): Expected keys:
+                'title' (str): Name of the document;
+
+        Raises:
+            Exception: If unable to create document.
+
+        Returns:
+            Dict[str, Any]: Expected keys:
+                'document_id' (str): Document ID;
+        """
         try:
             doc_id: str = ( # type: ignore
                 self.docs_service # type: ignore
@@ -50,34 +75,71 @@ class GoogleDocsCreate(GoogleDocsAction[Dict[str, Any], Dict[str, Any]]):
                 )
                 .execute()
             )
-            return {'doc_id': doc_id}
+            return {'document_id': doc_id}
         except Exception as e:
-            raise ValueError(f'Unable to create doc: {e}')
+            raise Exception(f'Unable to create document: {e}')
 
 
 class GoogleDocsWrite(GoogleDocsAction[Dict[str, Any], Dict[str, Any]]):
+    """
+    Write to document
+    """
     def execute(self, input_data: Dict[str, Any]) -> Dict[str, Any]:
+        """
+        Args:
+            input_data (Dict[str, Any]): Expected keys:
+                'document_id' (str): ID of the document. Document ID (can be found
+                    in url: https://docs.google.com/documents/d/***document_id***/edit#gid=0);
+
+                'actions' (Dict[str, Any]): Actions for updating the document.
+
+        Raises:
+            Exception: If unable to update document.
+
+        Returns:
+            Dict[str, Any]: Expected keys:
+                'document' (str): Updated document;
+        """
         try:
-            input_data["document"] = (
-                self.docs_service # type: ignore
-                .batchUpdate(
-                    documentId=input_data["document_id"], 
-                    body={'requests': input_data["actions"]}
-                ).execute()
-            )
-            return input_data
+            return {
+                "document": (
+                    self.docs_service # type: ignore
+                    .batchUpdate(
+                        documentId=input_data["document_id"], 
+                        body={'requests': input_data["actions"]}
+                    ).execute()
+                )
+            }
         except Exception as e:
-            raise ValueError(f'Unable to update doc: {e}')
+            raise Exception(f'Unable to update document: {e}')
 
 
 class GoogleDocsRead(GoogleDocsAction[Dict[str, Any], Dict[str, Any]]):
+    """
+    Read document
+    """
     def execute(self, input_data: Dict[str, Any]) -> Dict[str, Any]:
+        """
+        Args:
+            input_data (Dict[str, Any]): Expected keys:
+                'document_id' (str): ID of the document. Document ID (can be found
+                    in url: https://docs.google.com/documents/d/***document_id***/edit#gid=0);
+
+        Raises:
+            Exception: If unable to read document.
+
+        Returns:
+            Dict[str, Any]: Expected keys:
+                'document' (str): Document;
+        """
         try:
-            return (
-                self.docs_service # type: ignore
-                .get(
-                    documentId=input_data["document_id"]
-                ).execute()
-            )
+            return {
+                "document": (
+                    self.docs_service # type: ignore
+                    .get(
+                        documentId=input_data["document_id"]
+                    ).execute()
+                )
+            }
         except Exception as e:
-            raise ValueError(f'Unable to read doc: {e}')
+            raise ValueError(f'Unable to read document: {e}')
--- src/implementation/datasources/google_docs/schema.py
+++ src/implementation/datasources/google_docs/schema.py
@@ -3,8 +3,20 @@ from implementation.apis.google_cloud.schema import (
 )
 
 class GoogleDocsClientConfig(GoogleCloudClientConfig):
+    """
+    Google Documents default configuration
+    """
     scopes: list[str] = [
-        "https://www.googleapis.com/auth/documents" ##################################################
+        "https://www.googleapis.com/auth/documents"
     ]
+    """
+    Read and write acess to all documents
+    """
     service: str = "docs"
+    """
+    Google Documents service
+    """
     version: str = "v1"
+    """
+    API version
+    """
