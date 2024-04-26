fc1d6a5882db1ba419f11d7bc5d741bbe68ee119
mvy <mykytayaroshenko@gmail.com>
2024-04-26 22:29:51 +0300

    refactor: exceptions

--- src/core/exceptions.py
+++ src/core/exceptions.py
@@ -18,38 +18,43 @@ class InputDataKeyError(KeyError):
         super().__init__(f"Attribute '{key}' not found in input_data.")
 
 
-class InputSchemaNotFound(Exception):
-    def __init__(self, message: str="InputSchemaNotFound"):
-        self.message = message
-        super().__init__(self.message)
+# class InputSchemaNotFound(Exception):
+#     def __init__(self, message: str="InputSchemaNotFound"):
+#         self.message = message
+#         super().__init__(self.message)
 
 
-class OutputSchemaNotFound(Exception):
-    def __init__(self, message: str="OutputSchemaNotFound"):
-        self.message = message
-        super().__init__(self.message)
+# class OutputSchemaNotFound(Exception):
+#     def __init__(self, message: str="OutputSchemaNotFound"):
+#         self.message = message
+#         super().__init__(self.message)
 
 
-class ExecutionSchemaInvalidFirstComponent(Exception):
-    def __init__(self, message: str="First component should be executable or input"):
-        self.message = message
-        super().__init__(self.message)
+# class ExecutionSchemaInvalidFirstComponent(Exception):
+#     def __init__(self, message: str="First component should be executable or input"):
+#         self.message = message
+#         super().__init__(self.message)
 
 
-class ExecutionSchemaInvalidFlow(Exception):
-    def __init__(self, message: str="Last executable can not be null"):
-        self.message = message
-        super().__init__(self.message)
+# class ExecutionSchemaInvalidFlow(Exception):
+#     def __init__(self, message: str="Last executable can not be null"):
+#         self.message = message
+#         super().__init__(self.message)
+
+
+class ExecutionSchemaFailed(Exception):
+    def __init__(self, name: str, e: Exception):
+        super().__init__(f"{name}: Execution schema failed: {e}")
 
 
 class EvaluatorExecutionFailed(Exception):
-    def __init__(self, e: Exception):
-        super().__init__(f"Program failed! {e}")
+    def __init__(self, name: str, e: Exception):
+        super().__init__(f"{name}: {e}")
 
 
 class InavalidMemoryInstruction(ValueError):
     ...
 
 
-class IvalidInputDataValue(ValueError):
+class IvalidInputData(ValueError):
     ...
