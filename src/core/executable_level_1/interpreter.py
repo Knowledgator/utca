ffabbffd566e187ad22f4119644d385ac7c417f9
mvy <mykytayaroshenko@gmail.com>
2024-04-26 22:33:06 +0300

    refactor: Evaluator

--- src/core/executable_level_1/interpreter.py
+++ src/core/executable_level_1/interpreter.py
@@ -1,133 +1,111 @@
 from __future__ import annotations
 from typing import (
-    Dict, Any, List, Optional, Tuple, Union, TYPE_CHECKING
+    Any, Dict, List, Optional, Tuple, Union, TYPE_CHECKING
 )
 import logging
 
 from core.executable_level_1.component import Component
 from core.executable_level_1.schema import Transformable
+from core.exceptions import EvaluatorExecutionFailed
 if TYPE_CHECKING:
     from core.executable_level_1.memory import MemoryManager
 
-INPUT = "input"
 
-class EvaluatorConfigs:
+class Evaluator(Component):
     """
-    Configuration for evaluator
+    Manages context of execution
     """
-    logging_level: int
-    logging_handler: logging.Handler
-    logger: logging.Logger
-    fast_exit: bool
+    memory_manager: MemoryManager
+    schema: Component
 
     def __init__(
-        self,
-        name: str="Evaluator", 
+        self, 
+        schema: Component, 
         logging_level: int=logging.NOTSET,
         logging_handler: Optional[logging.Handler]=None,
-        fast_exit: bool=True
-    ):
+        fast_exit: bool=True,
+        memory_manager: Optional[MemoryManager]=None,
+        name: Optional[str]=None,
+    ) -> None:
         """
         Args:
-            name (str, optional): Identification name. Defaults to "Evaluator".
-            
+            schema (Component): Wrapped Component.
+
             logging_level (int, optional): Logging level. Defaults to logging.NOTSET.
+                See: https://docs.python.org/3/library/logging.html#logging-levels
             
             logging_handler (Optional[logging.Handler], optional): Handler that will be used.
-                Defaults to None.
+                If value equals to None, default will be created. Defaults to None.
+                See: https://docs.python.org/3/library/logging.html
 
             fast_exit (bool, optional): If set to True, after first unhandled exeception
                 execution will be terminated. Defaults to True.
+            
+            memory_manager (Optional[MemoryManager], optional): Manages data, that can be accesed 
+                in evaluator scope. If equals to None, default memory manager will be created.
+                Defaults to None.
 
+            name (Optional[str], optional): Name for identification.
+                If equals to None, class name will be used. Defaults to None.
         """
-        self.name = name
+        from core.executable_level_1.memory import MemoryManager
+        super().__init__(name)
         self.logging_level = logging_level
         self.logging_handler = logging_handler or logging.StreamHandler()
         self.logger = logging.getLogger(self.name)
         self.logger.setLevel(self.logging_level)
         self.logger.addHandler(self.logging_handler)
         self.fast_exit = fast_exit
-
-
-class Evaluator:
-    """
-    Manages context of execution
-    """
-    memory_manager: MemoryManager
-    schema: Component
-    cfg: EvaluatorConfigs
-
-    def __init__(
-        self, 
-        schema: Component, 
-        cfg: Optional[EvaluatorConfigs]=None,
-        memory_manager: Optional[MemoryManager]=None,
-    ) -> None:
-        """
-        Args:
-            schema (Component): Wrapped Component.
-
-            cfg (Optional[EvaluatorConfigs], optional): Evaluator configuration.
-                If value equals to None, default configuration will be created. 
-                Defaults to None.
-            
-            memory_manager (Optional[MemoryManager], optional): Manages data, that can be accesed 
-                in evaluator scope. If equals to None, default memory manager will be created.
-                Defaults to None.
-        """
-        from core.executable_level_1.memory import MemoryManager
-        self.cfg = cfg or EvaluatorConfigs()
         self.memory_manager = memory_manager or MemoryManager()
         self.schema = schema
 
 
-    def prepare_input(
-        self, 
-        input_data: Optional[Dict[str, Any]]=None
+    def __call__(
+        self, input_data: Transformable, evaluator: Optional[Evaluator]=None
     ) -> Transformable:
         """
-        Sets the initial input for the program.
+        Executes component with prepared data
 
         Args:
-            input_data (Optional[Dict[str, Any]], optional): Initial data. Defaults to None.
+            input_data (Transformable): Data for processing
 
+            evaluator (Optional[Evaluator], optional): Evaluator in context of wich component executed.
+                Defaults to None.
+
+        Raises: 
+            EvaluatorExecutionFailed: Reraised exception after logging to parent evaluator.        
+        
         Returns:
-            Transformable: Prepared data.
+            Transformable: Result of executed component.
         """
-        if input_data is not None:
-            return Transformable(input_data)
-        return Transformable({})
-
+        try:
+            return self.schema(input_data, self)
+        except Exception as e:
+            e = EvaluatorExecutionFailed(self.name, e)
+            if evaluator:
+                evaluator.log(logging.ERROR, e, exc_info=True)
+            raise e
+        
 
     def run(
-        self, input_data: Optional[Dict[str, Any]]=None
+        self, input_data: Optional[Dict[str, Any]]=None, evaluator: Optional[Evaluator]=None
     ) -> Dict[str, Any]:
         """
-        Execution of wrapped component
+        Run Component
 
         Args:
             input_data (Optional[Dict[str, Any]], optional): Data for processing. 
+                If equals to None, empty dict will be used for input_data. Defaults to None.
+
+            evaluator (Optional[Evaluator], optional): Evaluator in context of wich component executed.
                 Defaults to None.
 
         Returns:
             Dict[str, Any]: Result of execution.
         """
-        return self.eval(self.prepare_input(input_data)).extract()
-    
+        return self(self.prepare_input(input_data), evaluator).extract()
 
-    def eval(self, input_data: Transformable) -> Transformable:
-        """
-        Executes component with prepared data
 
-        Args:
-            input_data (Transformable): Prepared data.
-
-        Returns:
-            Transformable: Result of execution.
-        """
-        return self.schema(input_data, self)
-
-    
     def get_memory(
         self, 
         register: Transformable, 
@@ -135,7 +113,7 @@ class Evaluator:
         delete: bool=False
     ) -> Transformable:
         """
-        Add data from memory to register
+        Get data from memory and set to register
 
         Args:
             register (Transformable): Destination.
@@ -161,7 +139,7 @@ class Evaluator:
         set_key: str,
     ) -> None:
         """
-        Sets data from register to memory
+        Set data from register to memory
 
         Args:
             register (Transformable): Source.
@@ -179,7 +157,7 @@ class Evaluator:
         self, identifier: str
     ) -> None:
         """
-        Deletes specified key from memory
+        Delete specified key from memory
 
         Args:
             identifier (str): Key for deletion.
@@ -203,14 +181,14 @@ class Evaluator:
             
             exc_info (bool, optional): Include exception info. Defaults to False.
         """
-        self.cfg.logger.log(level, f"{self.cfg.name}: {msg}", exc_info=exc_info)
+        self.logger.log(level, f"{self.name}: {msg}", exc_info=exc_info)
 
     
     def create_child(
         self, schema: Component, child_name: str
     ) -> Evaluator:
         """
-        Creates evaluator in context of current evaluator
+        Create evaluator in context of current evaluator
 
         Args:
             schema (Component): Component to wrapp.
@@ -222,14 +200,12 @@ class Evaluator:
         """
         return Evaluator(
             schema,
-            cfg=EvaluatorConfigs(
-                name=f"{self.cfg.name}.{child_name}",
-                logging_level=self.cfg.logging_level,
-                logging_handler=self.cfg.logging_handler,
-                fast_exit=self.cfg.fast_exit
-            )
+            name=f"{self.name}.{child_name}",
+            logging_level=self.logging_level,
+            logging_handler=self.logging_handler,
+            fast_exit=self.fast_exit
         )
     
 
     def __repr__(self) -> str:
-        return f"{self.__class__.__name__}: {self.cfg.name} ({self.__dict__})"
+        return f"{self.__class__.__name__}: {self.name} ({self.__dict__})"
