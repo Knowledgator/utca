193b89baefec701f0992839b79f04870c8622c8b
mvy <mykytayaroshenko@gmail.com>
2024-05-14 20:39:11 +0300

    tests: refactor

--- tests/implementations/predictors/gliner_predictor/test_model.py
+++ tests/implementations/predictors/gliner_predictor/test_model.py
@@ -1,18 +1,17 @@
-from utca.core.executable_level_1.schema import Transformable
-from utca.implementation.predictors.gliner_predictor.predictor import (
+from utca.implementation.predictors import (
     GLiNERPredictor
 )
-from utca.implementation.predictors.gliner_predictor.schema import (
+from utca.implementation.predictors import (
     GLiNERPredictorConfig
 )
 def test_gliner():
     config = GLiNERPredictorConfig()
     predictor = GLiNERPredictor(config)
-    actual_ouput = predictor(Transformable({
+    actual_ouput = predictor.run({
         "texts": ["Earth is the most beautiful planet."],
         "labels": ["planet", "rocket", "star"],
         "threshold": 0.5,
-    })).extract()
+    })
 
     expected_ouput = {"label": "planet", "score": 0.9, "text": "Earth", "start": 0, "end": 5}
     assert (
