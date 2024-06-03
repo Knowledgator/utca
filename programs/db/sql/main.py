ee29edf5b9e6afd514394bd337ad142d299fd2ca
mvy <mykytayaroshenko@gmail.com>
2024-06-03 22:23:03 +0300

    feat: add neo4j graph database

--- programs/db/sql/main.py
+++ programs/db/sql/main.py
@@ -1,7 +1,6 @@
 from __future__ import annotations
+from typing import List, Optional
 
-from typing import List
-from typing import Optional
 from sqlalchemy import (
     ForeignKey, 
     String,
--- src/utca/implementation/datasources/db/__init__.py
+++ src/utca/implementation/datasources/db/__init__.py
@@ -1,10 +1,16 @@
 from utca.implementation.datasources.db.sqlalchemy.main import (
     SQLSessionFactory, BaseModel, SQLAction, SQLActionWithReturns
 )
+from utca.implementation.datasources.db.neo4j.main import (
+    Neo4jClient, Neo4jReadAction, Neo4jWriteAction
+)
 
 __all__ = [
     "SQLSessionFactory",
     "BaseModel",
     "SQLAction",
     "SQLActionWithReturns",
+    "Neo4jClient",
+    "Neo4jReadAction",
+    "Neo4jWriteAction",
 ]
