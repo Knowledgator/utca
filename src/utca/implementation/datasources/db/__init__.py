a330a9ced0815a19d39634d5fed9e3dc9b43b81a
mvy <mykytayaroshenko@gmail.com>
2024-06-04 19:09:20 +0300

    feat: add chromadb actions

--- src/utca/implementation/datasources/db/__init__.py
+++ src/utca/implementation/datasources/db/__init__.py
@@ -4,6 +4,20 @@ from utca.implementation.datasources.db.sqlalchemy.main import (
 from utca.implementation.datasources.db.neo4j.main import (
     Neo4jClient, Neo4jReadAction, Neo4jWriteAction
 )
+from utca.implementation.datasources.db.chroma.main import (
+    ChromaDBCollectionAddData, 
+    ChromaDBCollectionUpdate,
+    ChromaDBCollectionUpsert,
+    ChromaDBGetCollection,
+    ChromaDBCreateCollection,
+    ChromaDBGetOrCreateCollection,
+    ChromaDBDeleteCollection,
+    ChromaDBCollectionGet,
+    ChromaDBCollectionQuery,   
+)
+from utca.implementation.datasources.db.chroma.schema import (
+    ChromaDBEmbeddingFunctionComponent,
+)
 
 __all__ = [
     "SQLSessionFactory",
@@ -13,4 +27,14 @@ __all__ = [
     "Neo4jClient",
     "Neo4jReadAction",
     "Neo4jWriteAction",
+    "ChromaDBGetCollection",
+    "ChromaDBCreateCollection",
+    "ChromaDBGetOrCreateCollection",
+    "ChromaDBDeleteCollection",
+    "ChromaDBCollectionAddData", 
+    "ChromaDBCollectionUpdate",
+    "ChromaDBCollectionUpsert",
+    "ChromaDBCollectionGet",
+    "ChromaDBCollectionQuery",
+    "ChromaDBEmbeddingFunctionComponent",
 ]
