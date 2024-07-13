a9acb3ffddaa3fa892eb6db4b6c3284ce5cda034
MVYaroshenko <mykytayaroshenko@gmail.com>
2024-07-13 11:31:54 +0300

    tests: add tests for repeated or empty labels

--- tests/implementations/predictors/gliner_predictor/test_model.py
+++ tests/implementations/predictors/gliner_predictor/test_model.py
@@ -4,6 +4,7 @@ from utca.implementation.predictors import (
 from utca.implementation.predictors import (
     GLiNERPredictorConfig
 )
+
 def test_gliner():
     config = GLiNERPredictorConfig()
     predictor = GLiNERPredictor(config)
@@ -13,8 +14,41 @@ def test_gliner():
         "threshold": 0.5,
     })
 
+    actual_ouput["output"][0][0]["score"] = 0.9
     expected_ouput = {"label": "planet", "score": 0.9, "text": "Earth", "start": 0, "end": 5}
     assert (
-        expected_ouput["text"] 
-        == actual_ouput["output"][0][0]["text"]
+        expected_ouput
+        == actual_ouput["output"][0][0]
     )
+
+
+def test_gliner_repeated_or_empty_labels():
+    predictor = GLiNERPredictor()
+    actual_ouput = predictor.run({
+        "texts": ["Earth is the most beautiful planet."],
+        "labels": ["planet", "planet", "planet", "rocket", "star"],
+        "threshold": 0.5,
+    })
+    
+    actual_ouput["output"][0][0]["score"] = 0.9
+    expected_ouput = {"label": "planet", "score": 0.9, "text": "Earth", "start": 0, "end": 5}
+    assert (
+        expected_ouput
+        == actual_ouput["output"][0][0]
+    )
+
+    empty_res = predictor.run({
+        "texts": ["Earth is the most beautiful planet."],
+        "labels": [],
+        "threshold": 0.5,
+    })
+
+    assert empty_res["output"] == [[]]
+
+    empty_res = predictor.run({
+        "texts": ["Earth is the most beautiful planet.", "And another one"],
+        "labels": [],
+        "threshold": 0.5,
+    })
+
+    assert empty_res["output"] == [[], []]
